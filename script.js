229270527d983c1740c71902bc9109e0bc92c252
Anna Loshchakova <anna.loshchakova@aiesec.net>
2018-07-31 18:33:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,90 @@
-function getQuote() {
-//ajax нужен для запроса сервера по ссылке без перезагрузки страницы
-$.ajax({
-  headers: {'X-Mashape-Key': 'aB41byVgzpmsh2dMflSDeVQXr0Z0p12UPcBjsnfTJrcbuHAOuF',
-  'Content-Type': 'application/x-www-form-urlencoded',
-  Accept: 'application/json'},
-  // ссылка
-   url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=',
-  // Функция, которая будет вызвана в случае удачного завершения запроса к серверу
-  success: function(r) {
-      if (typeof r === 'string') {
-       r = JSON.parse(r); 
+﻿var quotes;
+//получаем объект с цитатами
+function getData() {
+  $.ajax({
+    headers: {
+      Accept: 'application/json'},
+      url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    success: function(data) {
+      if (typeof data === 'string') {
+        quotes = JSON.parse(data);
       }
-      currentQuote = r.quote;
-      currentAuthor = r.author;
-  // анимация текста
-     $("h1").animate({opacity: 0}, 500,
-      function() {
-        $(this).animate({opacity: 1}, 500);
-        $("h1>i").html(' '+ currentQuote);
-        });
-     $("p").animate({opacity: 0}, 500,
-      function() {
-        $(this).animate({opacity: 1}, 500);
-        $("p").html('- '+currentAuthor);
-   }); 
-  // анимация цвета
-    function generateColor() {
+      quotes= quotes.quotes;
+      displayQuote();
+    },
+    error: function (jqXHR, exception) {
+        // Что делать в случае ошибки
+        let msg = '';
+        if (jqXHR.status === 0) {
+            msg = 'Not connect.\n Verify Network.';
+        } else if (jqXHR.status == 404) {
+            msg = 'Requested page not found. [404]';
+        } else if (jqXHR.status == 500) {
+            msg = 'Internal Server Error [500].';
+        } else if (exception === 'parsererror') {
+            msg = 'Requested JSON parse failed.';
+        } else if (exception === 'timeout') {
+            msg = 'Time out error.';
+        } else if (exception === 'abort') {
+            msg = 'Ajax request aborted.';
+        } else {
+            msg = 'Uncaught Error.\n' + jqXHR.responseText;
+        }
+        alert(msg);
+        console.log(msg);
+    }
+  });
+}
+//берем рандомную цитату из объекта
+function getRandomQuote() {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+//берем рандомный цвет
+function generateRandomColor() {
   return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
-    var getRandomColor = generateColor();
-    $(".button, body, button").animate({backgroundColor: getRandomColor}, 1000);
-     $(".text-center").animate({color: getRandomColor}, 1000);
- }
-});
+//получаем цитаты из рандомной цитаты
+function displayQuote() {
+  let quote = getRandomQuote();
+  let currentQuote = quote.quote;
+  let currentAuthor = quote.author;
+  textAnimation(currentQuote, currentAuthor);
+  colorAnimation();
+}
+//анимируем размещаем цитату в .thumbnail 
+function textAnimation(currentQuote, currentAuthor) {
+  $("#quote").animate({opacity: 0}, 500,
+    function() {
+      $(this).animate({opacity: 1}, 500);
+      $("#quote").text(currentQuote);
+    });
+  $("#author").animate({opacity: 0}, 500,
+    function() {
+      $(this).animate({opacity: 1}, 500);
+      $("#name").text(currentAuthor);
+   }); 
+}
+//обновляем цвета на экране
+function colorAnimation() {
+  let getRandomColor = generateRandomColor();
+  $(".button, body, #newQuote").animate({backgroundColor: getRandomColor}, 1000);
+  $(".text-center").animate({color: getRandomColor}, 1000);
 }
 
 $(document).ready(function() {
-    getQuote();
-    $('button').on('click', getQuote);
+  //загружаем данные
+  getData();
+  //меняем цитаты по нажатию на кнопку
+  $('#newQuote').on('click', displayQuote);
   // кнопки поделиться
   //твиттер
-  $('#twitterShare').click( function (){
-var text='https://twitter.com/intent/tweet?hashtags=quotes&text="' + $('h1>i').text().slice(1)+'" '+$('p').text().slice(2);
-  $('#twitterShare').attr('href', text);
+  $('#twitterShare').click(function(){
+    let text = 'https://twitter.com/intent/tweet?hashtags=quotes&text="' + $('#quote').text()+'" '+$('#name').text();
+    $('#twitterShare').attr('href', text);
 });
   // фейсбук
-  $('#facebookShare').click( function (){
-var text='http://www.facebook.com/sharer.php?quote="' + $('h1>i').text().slice(1)+'" '+$('p').text().slice(2)+'&u=https://codepen.io/sohmet13/pen/baQLdw';
-  $('#facebookShare').attr('href', text);
+  $('#facebookShare').click(function(){
+    let text = 'http://www.facebook.com/sharer.php?quote="' + $('#quote').text()+'" '+$('#name').text()+'&u=https://codepen.io/sohmet13/full/baQLdw';
+    $('#facebookShare').attr('href', text);
+  });
 });
-    });
